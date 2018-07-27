916197eebd3a84a7d675d82a9a794892688df653
Štěpán Pešout <stepan@pesout.eu>
2018-07-27 13:39:12 +0200

    Comments translation

--- options_arguments_conf.sh
+++ options_arguments_conf.sh
@@ -1,36 +1,36 @@
 #! /bin/bash
 
-# SKRIPT ZPRACOVAVA KONFIGURACNI SOUBOR; UKLADA DO PROMENNYCH HODNOTY V NEM UVEDENE
+# THIS SCRIPT PROCESSES A CONF FILE AND STORES VARIABLE VALUES FROM THE FILE
 
 CONF_FILE="$1"
 
-# Overeni prav a existence u konfiguracniho souboru
+# Permissions and existence check (conf file)
 if [ ! -r "$CONF_FILE" -o ! -f "$CONF_FILE" ]; then
 	echo "CHYBA: Soubor neexistuje nebo nemate dostatecna prava"
 	exit 2
 fi
 
-# Odstraneni komentaru
+# Comments deletion
 eval $( sed 's/#.*$//g' "$CONF_FILE" |
 # Odstraneni whitespaces, pridani rovnitek; eval pro prirazeni do promennych
 sed 's/^[[:blank:]]*//g' | sed 's/[[:blank:]]*$//g' | grep -v '^$' |
 sed 's/\ /=" /' | sed -E 's/[[:blank:]]+//' | sed 's/$/"/g'
 ) 2>/dev/null || err=1
 
-# Overeni formatu konfiguracniho souboru
+# Conf file format check
 if [ $err -eq 1 ] 2>/dev/null; then
 	echo "CHYBA: Konfiguracni soubor ma nespravny format!"
 	exit 2
 fi
 
-# Funkce pro prepinac -v (verbose)
+# If verbose mode is on
 print_conf()
 {
-	# Vytazeni nazvu promennych z konfiguracniho s. a dotaz na jejich hodnoty
+	# Parsing variable names from the conf file and parsing it's vaules
 	declare -p $(echo $(
 	sed 's/#.*$//g' "$CONF_FILE" | sed 's/^[[:blank:]]*//g'	| 
 	grep -v '^$' | sed 's/[[:blank:]].*$//g')) |
-	#Odstraneni retezce "declare -- "	
+	# Deletion of the "declare --" string
 	cut -c 12-
 }
 
